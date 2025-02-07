60bb7abfb946cf7faae26aada760607a36614156
Pantelis I. <pantelisioa@yahoo.com>
2025-02-07 10:57:26 +0100

    Update SampleEn.py

--- SampleEn.py
+++ SampleEn.py
@@ -1,15 +1,17 @@
 import numpy as np
 import scipy as sp
+from scipy.spatial.distance import pdist
 
-def SampEn(x,m=1,r=0.2,N=len(x)):
+def SampEn(x,m=1,r=0.2,N=len(x), metric='chebyshev'):
     """
-    Calculates the sample entropy of the time series x.
+    Calculates the sample entropy of the time series x. Chebyshev is used as the distance metric.
 
     Input:
         x -- Timeseries array
         m -- Embedding Dimension, default m=1
         r -- Tolerance, default r=0.2*std
         N -- Length of the segment of the timeseries that we want to calculate the SampEn, default N=len(x)
+        metric -- Denotes the distance metric, default metric = 'chebyshev' (see scipy.spatial.distance.pdist)
 
     Output:
         sampen -- Sample Entropy
@@ -34,3 +36,28 @@ def SampEn(x,m=1,r=0.2,N=len(x)):
         x1[j,j:] = x[j:]
         x1[j] = np.roll(x[j],N-j)
         x1 = x1.T
+
+    dist0 = pdist(x0, metric)
+    dist1 = pdist(x1, metric)
+
+    # Get the number of distances that fullfill the tolerance criterion
+    crit0 = np.where(dist0<=tol)[0]
+    crit1 = np.where(dist1<=tol)[0]
+
+    # Set the A and B according to the SampEn definition
+    A = len(crit1)
+    B = len(crit0)
+
+    p = A/B
+    # Calculate the SampEn
+    sampen = -np.log(p) # RETURN
+
+    # Now lets calculate the standard error estimates
+    # This SEE is taken from the MATLAB code of Moorman and Lake
+    # https://www.physionet.org/content/sampen/1.0.0/matlab/#files-panel
+
+    var_p = (p*(1-p))/B # Variance of p
+    std_p = np.sqrt(var_p)
+    se = std_p/p # RETURN
+
+    return sampen, se
